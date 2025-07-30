5a358ebfeac9f3d1bf0e78838d97ac672df604db
Annanucki <anna.langauf@gmail.com>
2025-07-30 07:08:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,7 @@ const buttons = document.querySelectorAll('.button');
 
 const lastClickedHref = localStorage.getItem('lastClickedHref');
 
-if (lastClickedHref) {
-buttons.forEach(button => { 
-if (button.getAttribute('href') === lastClickedHref) {
-button.classList.add('active');
-}
-});
-}
 
-buttons.forEach(button => {
-button.addEventListener ('click', () => {
-const href = button.getAttribute('href');
-localStorage.setItem('lastClickedHref', href);
 });
 });
 });
